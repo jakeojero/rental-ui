e28b44ec5b7fe06701cd3ddfe385d32aa1f08516
davemeech <davemeech@gmail.com>
2018-03-11 22:40:56 -0400

    Unique login navbar

--- src/app/features/navbar/navbar.component.ts
+++ src/app/features/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertService } from '../alert/alert.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  user: string;
+  loggedIn = false;
+  constructor(private router: Router,
+    private alert: AlertService) { }
 
   ngOnInit() {
+    this.user = window.localStorage.getItem('username');
+    if (this.user) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
+
+  logout() {
+    window.localStorage.setItem('token', '');
+    window.localStorage.setItem('username', '');
+    window.localStorage.setItem('email', '');
+    this.loggedIn = false;
+    this.alert.info('You are now logged out.', 5000, true);
+    this.router.navigate(['home']);
   }
 
 }
