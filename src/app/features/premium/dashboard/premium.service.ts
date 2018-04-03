4d3a5049cc25c82ce119a0dc7e01aa5fc846f719
n1ke <jakeojero@hotmail.com>
2018-04-03 11:59:33 -0400

    tenants added

--- src/app/features/premium/dashboard/premium.service.ts
+++ src/app/features/premium/dashboard/premium.service.ts
@@ -15,6 +15,7 @@ export class PremiumService {
 
   user: User;
   properties = new BehaviorSubject<Property[]>([]);
+
   constructor(private navbar: NavbarService, private http: HttpClient, private spinner: SpinnerService, private alert: AlertService, private router: Router) {
     this.spinner.spin();
     this.navbar.getUser().subscribe(user => {
@@ -41,6 +42,47 @@ export class PremiumService {
     });
 
     return this.http.get(`/api/premium/properties?username=${this.user.username}`, {headers: headers});
+  }
+
+  getTenants() {
+    const headers = new HttpHeaders({
+      'X-AUTH-TOKEN': `${window.localStorage.getItem('token')}`
+    });
+
+    return this.http.get(`/api/tenants/user/${this.user.id}`, {headers: headers});
+  }
+
+  saveTenant(tenant) {
+    const headers = new HttpHeaders({
+      'X-AUTH-TOKEN': `${window.localStorage.getItem('token')}`
+    });
+
+    tenant.user = this.user;
+    return this.http.post(`/api/tenants/save`, tenant, {headers: headers});
+  }
+
+  deleteTenant(id) {
+    const headers = new HttpHeaders({
+      'X-AUTH-TOKEN': `${window.localStorage.getItem('token')}`
+    });
+
+    return this.http.delete(`/api/tenants/${id}`, {headers: headers});
+  }
+
+  getExpenses(propertyId) {
+    const headers = new HttpHeaders({
+      'X-AUTH-TOKEN': `${window.localStorage.getItem('token')}`
+    });
+
+    return this.http.get(`/api/expenses/${propertyId}`, {headers: headers});
+  }
+
+  saveExpense(expense) {
+    const headers = new HttpHeaders({
+      'X-AUTH-TOKEN': `${window.localStorage.getItem('token')}`
+    });
+
+    return this.http.post(`/api/expenses/save`, expense, {headers: headers});
   }
 
   getListOfProperties() {
--- src/app/features/premium/dashboard/tenants/tenants.component.ts
+++ src/app/features/premium/dashboard/tenants/tenants.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { PremiumService } from '../premium.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertService } from '../../../alert/alert.service';
+import { SpinnerService } from '../../../spinner/spinner.service';
 
 @Component({
   selector: 'app-tenants',
@@ -9,12 +12,67 @@ import { PremiumService } from '../premium.service';
 export class TenantsComponent implements OnInit {
 
   properties = <any>[];
-  constructor(private premiumService: PremiumService) { }
+  tenants = <any>[];
+  tenantForm: FormGroup;
+
+  constructor(private premiumService: PremiumService,
+              @Inject(FormBuilder) fb: FormBuilder,
+              private alert: AlertService,
+              private spinner: SpinnerService) { 
+
+               this.tenantForm = fb.group({
+                  firstname: ['', Validators.required],
+                  lastname: ['', Validators.required],
+                  property: ['', Validators.required]
+               });
+  }
 
   ngOnInit() {
+    this.spinner.spin();
     this.premiumService.getListOfProperties().subscribe(
       properties => {
         this.properties = properties;
+      }
+    );
+    
+    this.premiumService.getTenants().subscribe(
+      (response: Array<any>) => {
+        this.spinner.hide();
+        this.tenants = [...response];
+
+      },
+      err => {
+        this.alert.error('There was an error retrieving tenants', 5000, true);
+      }
+    )
+  }
+
+  saveTenant() {
+    this.spinner.spin();
+    this.premiumService.saveTenant(this.tenantForm.value).subscribe(
+      response => {
+        this.spinner.hide();
+        this.tenants.push(response);
+        this.tenantForm.reset();
+      },
+      err => {
+        this.spinner.hide();
+        this.alert.error('There was an error saving that tenant! Please try again.', 5000, false);
+      }
+    )
+  }
+
+  deleteTenant(tenant) {
+    this.spinner.spin();
+    this.premiumService.deleteTenant(tenant.id).subscribe(
+      response => {
+        this.spinner.hide();
+        this.alert.info(`Deleted ${tenant.firstname}`, 5000, true);
+        this.tenants.splice(this.tenants.indexOf(tenant), 1);
+      },
+      error => {
+        this.spinner.hide();
+        this.alert.error(`Error Deleting ${tenant.firstname}`, 5000, true);
       }
     )
   }
